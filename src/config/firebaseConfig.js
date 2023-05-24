1815e004813ca032c43825ae069daeec2a8543ef
Ankit <114883565+Ankitsatyam@users.noreply.github.com>
2023-05-24 12:19:34 +0530

    Add files via upload

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDPwgxK8fHlcgcXDB9IQe9fV8Pw1BLoVo8",
+    authDomain: "crypto-tracker-e48c5.firebaseapp.com",
+    projectId: "crypto-tracker-e48c5",
+    storageBucket: "crypto-tracker-e48c5.appspot.com",
+    messagingSenderId: "537331417249",
+    appId: "1:537331417249:web:3cd8cf0bb618402742b0e0",
+    measurementId: "G-6KN54V2MS0"
+  };
+
+  export default firebaseConfig;
